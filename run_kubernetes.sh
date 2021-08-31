08409645a8fd5f1becac0e3c2b68187712b24721
Vijay Ramachandran <r.vijayit@gmail.com>
2021-08-31 03:33:44 +0000

    Committing CircleCi config.yml
    	new file:   .circleci/config.yml
    	deleted:    minikube-linux-amd64
    	modified:   output_txt_files/docker_out.txt
    	modified:   output_txt_files/kubernetes_out.txt
    	modified:   run_docker.sh
    	modified:   run_kubernetes.sh
    	modified:   upload_docker.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=vijayramachandran/dockerproj
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run latest --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward latest 8000:80
